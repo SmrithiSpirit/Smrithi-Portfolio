48ea064b11461b436939bef4f71ac735d423cbe5
Smrithi B Ravikumar <smrithibr.br@gmail.com>
2026-04-10 23:20:49 +0530

    changes made on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import { HashRouter } from 'react-router-dom';
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProjectDetails from "./pages/ProjectDetails";
 import CaseStudyFocusMate from "./pages/CaseStudyFocusMate";
@@ -14,25 +14,45 @@ import CaseStudySaveEasy from "./pages/CaseStudySaveEasy";
 
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
+// const App = () => (
+//   <QueryClientProvider client={queryClient}>
+//     <TooltipProvider>
+//       <Toaster />
+//       <Sonner />
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Index />} />
+//           <Route path="/projects/:projectId" element={<ProjectDetails />} />
+//           <Route path="/case-studies/focusmate" element={<CaseStudyFocusMate />} />
+//           <Route path="/case-studies/campusconnect" element={<CaseStudyCampusConnect />} />
+//           <Route path="/case-studies/saveeasy" element={<CaseStudySaveEasy />} />
+//           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+//         </Routes>
+//       </BrowserRouter>
+//     </TooltipProvider>
+//   </QueryClientProvider>
+// );
+
+// export default App;
+// Replace <BrowserRouter> with <HashRouter>
+
+const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects/:projectId" element={<ProjectDetails />} />
           <Route path="/case-studies/focusmate" element={<CaseStudyFocusMate />} />
           <Route path="/case-studies/campusconnect" element={<CaseStudyCampusConnect />} />
           <Route path="/case-studies/saveeasy" element={<CaseStudySaveEasy />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
-
-export default App;
