601637ffd42d556b02e2aa9072550c65166bdb4b
Smrithi B Ravikumar <smrithibr.br@gmail.com>
2026-04-10 12:36:21 +0530

    Added portfolio project files

---
+++ src/pages/Index.tsx
@@ -0,0 +1,31 @@
+import { Navigation } from '@/components/Navigation';
+import { HeroSection } from '@/components/HeroSection';
+import { AboutSection } from '@/components/AboutSection';
+import { ExperienceSection } from '@/components/ExperienceSection';
+import { ProjectsSection } from '@/components/ProjectsSection';
+import { CaseStudiesSection } from '@/components/CaseStudiesSection';
+import { SkillsSection } from '@/components/SkillsSection';
+import { ServicesSection } from '@/components/ServicesSection';
+import { ContactSection } from '@/components/ContactSection';
+import { Footer } from '@/components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Navigation />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <CaseStudiesSection />
+        <ProjectsSection />
+        <ExperienceSection />        
+        {/* <SkillsSection /> */}
+        {/* <ServicesSection /> */}
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
