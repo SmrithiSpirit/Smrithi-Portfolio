e29858dc8f02e44de2c5f4305c1d0d7a5e7a4be2
Smrithi B Ravikumar <smrithibr.br@gmail.com>
2026-04-10 22:37:58 +0530

    Fix BrowserRouter import

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,48 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// export default defineConfig(({ mode }) => ({
+//   base: mode === "build" ? "/Smrithi-Portfolio/" : "/",
+
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+
+//   plugins: [
+//     react(),
+//     mode === "development" && componentTagger()
+//   ].filter(Boolean),
+
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "build" ? "/Smrithi-Portfolio/" : "/",
-
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/Smrithi-Portfolio/" : "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
-    },
-  },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
